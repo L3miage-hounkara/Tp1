659b556d3c01e4e55379fba918c458ea5b854384
hounkara <91179744+hounkara@users.noreply.github.com>
2024-03-05 00:14:50 +0100

    test commit

--- server/src/main/java/fr/uga/l3miage/tp1/exo1/models/CarEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/CarEntity.java
@@ -1,2 +1,28 @@
-package fr.uga.l3miage.tp1.exo1.models;public class CarEntity {
+package fr.uga.l3miage.tp1.exo1.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="car_porshe")
+public class CarEntity {
+    @Column(name = "immat",length = 8)
+    private String immatriculation;
+    //attribut immatriculation coorespond a l'attribut immat
+
+    
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
 }
